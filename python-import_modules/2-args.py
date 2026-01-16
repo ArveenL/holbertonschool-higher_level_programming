f401e05cdec46b17ebb75ec06faaecada8c69fd6
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-16 20:38:16 +0400

    2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -12,14 +12,11 @@ if __name__ == "__main__":
     args_count = len(args)
 
     if (args_count == 0):
-        print("no arguments.")
+        print("0 arguments.")
     elif (args_count == 1):
         print("1 argument:")
     else:
         print("{} arguments:".format(args_count))
 
     for i in range(args_count):
-        print(str[i + 1] + ": " + args[i])
-        
-
-    
+        print(str(i + 1) + ": " + args[i])
