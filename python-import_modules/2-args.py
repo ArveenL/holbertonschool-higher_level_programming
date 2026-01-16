b4b51323cee82729533f61afd598ff5dcf7c4a49
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-16 20:45:16 +0400

    2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,13 +2,13 @@
 import sys
 
 if __name__ == "__main__":
-    
-    #not counting program's name, 
-    #counting only arguments
+
+    # not counting program's name,
+    # counting only arguments
     args = sys.argv[1:]
 
-    #goes one by one through list of,
-    #arguments and returns its count
+    # goes one by one through list of,
+    # arguments and returns its count
     args_count = len(args)
 
     if (args_count == 0):
