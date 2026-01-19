f92864ab27d6ea1f4c3b3303681b7bfb54845710
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-19 13:44:30 +0400

    2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,8 +2,10 @@
 
 def replace_in_list(my_list, idx, element):
     if (idx < 0) or (idx >= len(my_list)):
-        return my_list
-
-        else:
+        return my_list 
+    
+    #note: return exits the function,
+    #so no need for else below
+        
         my_list[idx] = element
         return my_list
