d6343dbc5883d8120a6c5178dded84fcf6816c0a
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-19 14:07:38 +0400

    3

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -5,4 +5,4 @@ def print_reversed_list_integer(my_list=[]):
     if my_list if not None: # check if variable my_list hold a value
         reverse_array = my_list[::-1]
         for i in reverse_array:
-            print("{:d}"format.my_list(i))
+            print("{:d}".format(i))
