358d21b9bb2cc528af7f3a0fbd9c5e6e0b59248a
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-19 13:23:49 +0400

    2

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def replace_in_list(my_list, idx, element):
+
+    for item in my_list:
+        if (idx < 0):
+            return my_list
+        
+        elif (idx > len(my_list)):
+            return my_list
+        
+        else:
+            my_list(idx) = my_list(element)
