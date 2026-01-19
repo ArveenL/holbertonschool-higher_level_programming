c811c5f0dafe6d51757eab63e7a089dd834e56e1
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-19 14:57:13 +0400

    4

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    if idx < 0 or idx >= len(my_list):
+        return my_list
+        
+        
+    new_list = my_list[:]  # replace old list with new list
+    new_list[idx] = element  # swap index x in new list with element
+    return new_list 
