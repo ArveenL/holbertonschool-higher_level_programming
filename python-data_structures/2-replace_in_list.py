072af6517c39f5db2343f689f9b73963699761c8
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-19 13:35:33 +0400

    2

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -3,12 +3,9 @@
 def replace_in_list(my_list, idx, element):
 
     for item in my_list:
-        if (idx < 0):
-            return my_list
-        
-        elif (idx > len(my_list)):
+        if (idx < 0) or (idx >= len(my_list)):
             return my_list
         
         else:
-             element = my_list(idx)
-             return my_list(element)
+            my_list(idx) = element
+            return my_list
