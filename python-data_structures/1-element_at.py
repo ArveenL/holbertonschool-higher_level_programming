283b372ead2c70950a1f0d64d4c8775e4ee62940
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-19 11:46:21 +0400

    1

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,11 +3,11 @@
 def element_at(my_list, idx):
 
     for item in my_list:
-        if(idx < 0):
+        if (idx < 0):
             return None
-        
-        elif(idx >= len(my_list)):
+
+        elif (idx >= len(my_list)):
             return None
-        
+
         else:
             return my_list[idx]
