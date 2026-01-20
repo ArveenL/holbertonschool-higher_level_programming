1cde5dd0adabddcb5a0d94c13605b4103d8ff405
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-20 14:33:37 +0400

    10

--- python-data_structures/10-divisible_by_2.py
+++ python-data_structures/10-divisible_by_2.py
@@ -5,7 +5,7 @@ def divisible_by_2(my_list=[]):
     by_two = []
 
     for i in my_list:
-        if i / 2 == 0:
+        if i % 2 == 0:
             by_two.append(True)
         else:
             by_two.append(False)
