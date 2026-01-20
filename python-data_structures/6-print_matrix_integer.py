e1ad4dea44c323b7342e4b0289ff5b0cc3056458
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-20 10:28:05 +0400

    6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,7 +2,6 @@
 
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        for item in row:
-            # Format each integer to take at least 4 spaces for alignment
-            print(f"{item:4}", end=" ")
-        print()  # Print a new line after each row
+        for value in row:
+            print("{:d}".format(value), end=" ")
+        print()  # print new line after each row
