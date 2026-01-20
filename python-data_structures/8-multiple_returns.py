8f63d24302ffcaee5034453f719141e32b9112ac
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-20 13:46:41 +0400

    8

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-
-    first_char = 0
-
     if len(sentence) > 0:
         first_char = sentence[0]
-
     else:
-        sentence[0] = None
+        first_char = None
 
     return (len(sentence), first_char)
