3a5155c928cc1a58c4941055b3a47f19ebf5d69e
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-20 14:43:53 +0400

    11

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,14 +1,6 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-    if idx < 0 or idx >= len(my_list):
-        return my_list
+    if idx < 0 or idx >= len(my_list):  # invalid index
+        return my_list  # return unchanged list
     return my_list[:idx] + my_list[idx+1:]
-
-my_list = [1, 2, 3, 4, 5]   # define the list
-
-my_list = delete_at(my_list, 3)  # delete index 3
-print(my_list)  # [1, 2, 3, 5]
-
-my_list = delete_at(my_list, 10)  # try invalid index
-print(my_list)  # [1, 2, 3, 5]
