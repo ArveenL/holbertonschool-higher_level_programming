145d35762702ac65530ab0380731e912a096ee79
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-20 13:12:37 +0400

    8

---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,11 @@
+def multiple_returns(sentence):
+
+    first_char = 0
+
+    if len(sentence) != 0:
+        sentence[0] = first_char
+
+    else:
+        sentence[0] = None
+
+    return (len(sentence), first_char)
