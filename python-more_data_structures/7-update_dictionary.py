958d04cc27c894bdcbed310f83121c6d9f331b46
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-21 10:54:42 +0400

    update key/value in dict

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    a_dictionary[key] = value
+
+# dictionaries are permutable i.e you can modify
+# or add keys/values as you go, and if it doesn't
+# exist, it will create a new one
