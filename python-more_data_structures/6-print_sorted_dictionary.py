961a40d5fcfa9ba12c7eca3f05b7263f666180bf
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-21 10:00:57 +0400

    6 - sorted keys

---
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def print_sorted_dictionary(a_dictionary):
+
+    for variable in sorted(a_dictionary):
+        print(f"{variable} : {a_dictionary[variable]}")
+
+# 1. for variable in sorted(a_dictionary) means LOOP through
+# the keys in dictionary called a_dictionary, sorted by
+# alphabetical order but dominated by ASCII fifo
+
+# 2. a_dictionary[variable] means the value held by key x
