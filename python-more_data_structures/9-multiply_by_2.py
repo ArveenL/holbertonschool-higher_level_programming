ff81bf61f59a06ed93d4dc314144fe5312a1603e
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-21 11:35:57 +0400

    testing 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -4,7 +4,7 @@ def multiply_by_2(a_dictionary):
 
     new_dictionary = {}
 
-    for index, VALUE in a_dictionary:
+    for index, VALUE in a_dictionary.items:
         new_dictionary[VALUE] = VALUE * 2
 
     return new_dictionary
