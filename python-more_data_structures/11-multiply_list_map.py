7f66750cba56386cb2b5fef861e7a533a0771c5a
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-21 15:25:17 +0400

    try 11

---
+++ python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def multiply_list_map(my_list=[], number=0):
+    return (map(lambda unknown : unknown * number, my_list)
+
+
+
+
+
+# 1. my_list=[] meaning we have a list with unknown
+# number of items
+
+# 2. number = 0 means the number we ought to multiply
+# our items with, 0 meaning we're starting it as 0, but
+# when running the function, it can be any number
