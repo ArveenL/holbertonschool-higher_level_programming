9af9def2f5603694735f53dc67fc5ea36fffdb00
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-21 10:56:50 +0400

    update key/value in dict

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -3,6 +3,10 @@
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
 
-# dictionaries are permutable i.e you can modify
+    return a_dictionary
+
+# 1. dictionaries are permutable i.e you can modify
 # or add keys/values as you go, and if it doesn't
 # exist, it will create a new one
+
+# 2. return dictionary to say "please find updated dict"
