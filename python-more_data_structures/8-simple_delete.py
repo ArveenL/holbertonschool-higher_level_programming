97ddf86d1bfa7a8c112f426a803904f46803a4cb
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-21 11:18:25 +0400

    loop and delete key using del( )

--- python-more_data_structures/8-simple_delete.py
+++ python-more_data_structures/8-simple_delete.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 def simple_delete(a_dictionary, key=""):
-    if key in a_dictionary:  #1
-        del(a_dictionary[key])  #2
+    if key in a_dictionary:  # 1
+        del (a_dictionary[key])  # 2
 
-    return a_dictionary  #3
+    return a_dictionary  # 3
 
 # 1. loop through all keys until it finds key to be
 #     used when function simple_delete is being called
