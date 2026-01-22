417d93cb45feef461238db59be9711c4534da3fc
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-22 20:22:37 +0400

    0 - use try except & loops

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    
+    count = 0
+    
+    for i in range(x):
+        try:
+            print(my_list, end="")
+            count = count + 1
+        except IndexError:
+            break
+        print()
+        return count
+    
+    # 1. count = tracks actual amount of x-element
+    #           + 1 with it every time we've 
+    #           printed 1 x-element
+
+    # 2. exept IndexError is like saying "until NULL"
+    #   in C language
+
+    # 3.  2nd print() is to print new line as per the question
+
+    # 4. return count to confirm actual amount of x element printed
