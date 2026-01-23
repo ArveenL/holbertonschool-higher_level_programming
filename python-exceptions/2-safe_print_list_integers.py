6da9db5262f3c9550a81a0c31379e25167176910
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-23 10:00:52 +0400

    2 intro to continue

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -8,12 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
             count = count + 1
     
         except (ValueError, TypeError):
-            continue  # skip non-integers
-    
-        except IndexError:
-            break     # stop if x > list length
-    
+            continue
+        
     print()
     return count
-
-# nothing 
