14473958d13de64bb77797aabc1df03cfa61247e
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-23 10:26:33 +0400

    oppa pycode style

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,33 +1,33 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-    
     count = 0
-    
+
     for i in range(x):
         try:
             print(my_list[i], end="")
-            count = count + 1
-        
+            count += 1
         except IndexError:
             break
-        
+
     print()
     return count
 
-############_Notes_################################
+    # _Notes_ ################################
     # 1. count = tracks actual amount of x-element
-    #           + 1 with it every time we've 
-    #           printed 1 x-element
-
-    # 1.5. note printing my_list[i] to print element
-    # per element and not the whole list at once
-
+    #    +1 every time we have printed one element
+    #
+    # 1.5 note: printing my_list[i] prints element
+    #     per element, not the whole list at once
+    #
     # 2. except IndexError is like saying "until NULL"
-    #   in C language
-
-    # 3.  2nd print() is to print new line as per the question
-
-    # 4. return count to confirm actual amount of x element printed
-
-    # 5. indent 2nd print and return count same column as for loop
+    #    in C language
+    #
+    # 3. second print() is to print a new line
+    #    as required by the question
+    #
+    # 4. return count to confirm actual amount
+    #    of x elements printed
+    #
+    # 5. indent second print() and return count
+    #    at the same level as the for loop
