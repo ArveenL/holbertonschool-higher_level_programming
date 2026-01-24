e0f2fc75f8bfa4603bde3c3362f5d9beef14f486
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-24 18:37:09 +0400

    0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""Module for integer addition with type checking."""
-
 def add_integer(a, b=98):
-    """Return the addition of a and b."""
-    if not isinstance(a, (int, float)):
+    if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
+    if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    return int(a) + int(b)
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testmod()
-# test
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return (a + b)
