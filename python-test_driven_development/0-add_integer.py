e8ccaff2000c8760b394c605500cf3bcb928c183
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-24 15:51:53 +0400

    0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,14 +1,24 @@
 #!/usr/bin/python3
-"""add two integers"""
+"""Add two integers.
 
+>>> add_integer(1, 2)
+3
+>>> add_integer(2.5, 3.5)
+5
+>>> add_integer(4)
+102
+>>> add_integer(4, "School")
+Traceback (most recent call last):
+    ...
+TypeError: b must be an integer
+>>> add_integer(None)
+Traceback (most recent call last):
+    ...
+TypeError: a must be an integer
+"""
 def add_integer(a, b=98):
-    """Return the addition of a and b."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
         raise TypeError("b must be an integer")
-    
     return int(a) + int(b)
-
-# isintance checks if variable is of type mentioned 
-# x
