fe1c06a6b1daba6150b5e4cec71d1c07a82e592c
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-28 11:00:56 +0400

    3

---
+++ python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Defines a Square class with a validated private size attribute."""
+
+
+class Square:
+    """Represents a square defined by its size."""
+
+    def __init__(self, size=0):
+        """
+        Initialize a Square instance with a given size.
+
+        :param size: size of the square (default is 0)
+        :raises TypeError: if size is not an integer
+        :raises ValueError: if size is less than 0
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Return the area of the square"""
+
+        return self.__size * self.__area
