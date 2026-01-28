899547c53fa2436289c68a8330c5a168b5fb6fe7
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-28 10:56:29 +0400

    2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,25 +1,22 @@
 #!/usr/bin/python3
-
-"""class Square"""
+"""Defines a Square class with a validated private size attribute."""
 
 
 class Square:
+    """Represents a square defined by its size."""
 
     def __init__(self, size=0):
-        
-        """private __size object found after 'self
-    
-        Initialize a new Square instance.
+        """
+        Initialize a Square instance with a given size.
 
-        :param self: instance being created
-        :param size: size of the square (default 0)
+        :param size: size of the square (default is 0)
         :raises TypeError: if size is not an integer
         :raises ValueError: if size is less than 0
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-   
+
         if size < 0:
             raise ValueError("size must be >= 0")
-   
+
         self.__size = size
