3939eb635760f13e25f7ba772cdb335efe7aee81
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-28 10:40:31 +0400

    2

---
+++ python-classes/2-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""class : Square"""
+
+
+class Square:
+   """
+    Initialize a new Square instance.
+
+    :param self: instance being created
+    :param size: size of the square (default 0)
+    :raises TypeError: if size is not an integer
+    :raises ValueError: if size is less than 0
+    """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+   
+        if size < 0:
+            raise ValueError("size must be >= 0")
+   
+    self.__size = size
