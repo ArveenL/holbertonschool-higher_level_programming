c0cfbb23ea47bc053f3398e3a93441422cd1813c
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-28 10:13:00 +0400

    1

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -4,13 +4,14 @@
 class : Square
 """
 
+
 class Square:
     """private __size object found after 'self'"""
 
     def __init__(self, size):
         """
         Docstring for _init_
-        
+
         :param self: Instance being created
         :param size: Size of square(private information)
         """
