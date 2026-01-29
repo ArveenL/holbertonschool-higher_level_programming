3144d39cdac2aeacd84282c4b5e5d9581fd9e155
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-29 20:25:19 +0400

    x

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,22 +1,19 @@
 #!/usr/bin/python3
-"""Defines a Square class with a validated private size attribute."""
+
+"""declare class named Square"""
 
 
 class Square:
-    """Represents a square defined by its size."""
+    """add value to object, check type &
+    value of object, return Error accordingly"""
 
     def __init__(self, size=0):
-        """
-        Initialize a Square instance with a given size.
-
-        :param size: size of the square (default is 0)
-        :raises TypeError: if size is not an integer
-        :raises ValueError: if size is less than 0
-        """
-        if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-
-        if size < 0:
-            raise ValueError("size must be >= 0")
-
         self.__size = size
+
+    if type(size) != int:
+        TypeError("size must be an integer")
+
+    if size < 0:
+        TypeError("size must be >= 0")
+
+#adding notes to prevent ds_store bullshit
