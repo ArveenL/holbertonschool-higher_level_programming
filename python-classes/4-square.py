eaafc8edabbdf0bc918aa74e59f2678b7b56355c
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-29 21:21:11 +0400

    x

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python3
-
-"""
-Defines a Square class with a private size attribute,
-getter/setter validation, and area computation.
-"""
+"""declare class named Square"""
 
 
 class Square:
     """
-    Initialize the square with an optional size
-    """
+    new: SETTER = prevents attribute from being READ
+    freely
+
+    new: GETTER = prevents attribute from being MODIFIED
+    freely"""
+
     def __init__(self, size=0):
-        self.size = size
+        self.__size = size
 
     @property
-    def size(self):
-        """return the current size of the square"""
+    def size(self):         # getter
         return self.__size
 
     @size.setter
-    def size(self, value):
-        """set the size of the squre with type & value validations"""
+    def size(self, value):  # setter
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
 
         if value < 0:
             raise ValueError("size must be >= 0")
 
-        self.__size = value
-
     def area(self):
-        """return area of square"""
         return self.__size * self.__size
+
+# using 'isinstance' instead of 'type(size) != int'
+# because of some inheritance thingy
