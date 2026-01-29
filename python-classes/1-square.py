5ee9a0e479413c8fe39c95544a70cfdf2509435c
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-01-29 20:17:53 +0400

    x

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
 
-"""declare class named square"""
+"""declare class named Square"""
 
 
+class Square:
+    """Instantiation of first object,
+    make it private & no type/value yet"""
+
+    def __init__(self, size):
+        self.__size = size
