ee29b388a03e009a507fe6d11b5eb22ec9b0b0dd
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-02-02 20:49:09 +0400

    x

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""function that returns True if the object is an instance of a class
+that inherited (directly or indirectly) from the specified class; 
+otherwise False."""
+
+
+def inherits_from(obj, a_class):
+    """"Args
+            obj: object to check
+            a_class: class to check
+
+        Return: True/False"""
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
