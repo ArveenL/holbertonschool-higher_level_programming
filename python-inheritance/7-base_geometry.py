cebccdc1e65c15c6e7d6a7b0fc1ed7a163d88509
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-02-03 12:46:16 +0400

    x

---
+++ python-inheritance/7-base_geometry.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+""""new: Public instance method: def integer_validator(self, name, value): 
+that validates value:
+you can assume name is always a string
+
+if value is not an integer: 
+raise a TypeError exception, with the message <name> must be an integer
+
+if value is less or equal to 0: 
+raise a ValueError exception with the message <name> must be greater than 0"""
+
+
+class BaseGeometry:
+    """docstring"""
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if value != type(int):
+            raise TypeError("<name> must be an integer")
+        
+        if value <= 0:
+            raise ValueError("<name> must be greater than 0")
