550096adb52c424d216d5a42016d3db79826d481
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-02-03 14:15:21 +0400

    x

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Square class that inherits from Rectangle."""
+
+Rectangle = __import__('8-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class inheriting from Rectangle."""
+
+    def __init__(self, size):
+        """Initialize Square with validated size."""
+        self.integer_validator("size", size)
+        self.__size = size
+        # A square is a rectangle with width == height == size
+        super().__init__(size, size)
+
+    def area(self):
+        """Compute the area of the square."""
+        return self.__size * self.__size
+
+    def __str__(self):
+        """Return the square description: [Square] <width>/<height>."""
+        return f"[Square] {self.__size}/{self.__size}"
