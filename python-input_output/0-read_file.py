22ac1a6f20f968bd5c9a29039c6cc180e953b337
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-02-10 22:02:59 +0400

    x

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -2,4 +2,4 @@
 
 def read_file(filename=""):
     with open(filename, encoding="utf-8") as justavariable:  # opens file and auto-close it
-        print(justavariable.read, end="")
+        print(justavariable.read(), end="")
