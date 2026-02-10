c01d7e6bf866c651bf9cabc16bb14d1859e956a6
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-02-10 22:21:34 +0400

    x

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,6 +5,7 @@ Docstring for python-input_output.1-write_file
 
 
 def write_file(filename="", text=""):
+    """suckstring!"""
     with open("my_first_file.txt", "w", encoding="UTF8") as f:
         n = f.write("This School is so cool!\n")
         return n
