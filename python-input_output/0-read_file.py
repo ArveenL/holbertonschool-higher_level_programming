aaeaa09bf41cda9d5251fbfb1202fa5d28de3d35
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-02-10 22:08:53 +0400

    x

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -5,6 +5,9 @@ Docstring for python-input_output.0-read_file
 
 
 def read_file(filename=""):
+    """
+    suckstring!
+    """
     with open(filename, encoding="utf-8") as justavariable:  
         print(justavariable.read(), end="")
 
