2bfbe11c440401a7c464ad806893a9d03c5daa36
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-02-10 22:09:36 +0400

    x

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -8,7 +8,7 @@ def read_file(filename=""):
     """
     suckstring!
     """
-    with open(filename, encoding="utf-8") as justavariable:  
+    with open(filename, encoding="utf-8") as justavariable:
         print(justavariable.read(), end="")
 
 # "with open() as variable" -> opens THEN auto-close file
