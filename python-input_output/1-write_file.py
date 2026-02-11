8b30f99d582e6c69b09b06b89d3e0ad22bc1aee3
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-02-11 09:14:56 +0400

    x

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,6 +6,8 @@ Docstring for python-input_output.1-write_file
 
 def write_file(filename="", text=""):
     """suckstring!"""
-    with open("my_first_file.txt", "w", encoding="UTF8") as f:
+    with open(filename, "w", encoding="UTF8") as f:
         n = f.write("This School is so cool!\n")
         return n
+
+# p.s write_file gives the file name in the main
