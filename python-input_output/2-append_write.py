2b25338b50518890bf85e466343e9b96c0af580a
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-02-11 09:50:38 +0400

    x

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -7,10 +7,10 @@ Docstring for python-input_output.2-append_write
 def append_write(filename="", text=""):
     """
     Docstring for append_write
-    
+
     :param filename: Description
     :param text: Description
     """
     with open(filename, "a", encoding="UTF8") as f:
-              n = f.write(text)
-              return n
+        n = f.write(text)
+        return n
