7199779db6eb1f557d681dece30093da88f75745
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-02-15 20:58:09 +0400

    x

--- python-input_output/8-class_to_json.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""Class to JSON module"""
-
-
-def class_to_json(obj):
-    """Return the dictionary description of an object for JSON serialization"""
-    return obj.__dict__
-
