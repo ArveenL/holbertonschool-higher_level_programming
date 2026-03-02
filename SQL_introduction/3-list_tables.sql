f4e7dab0e9673749db1e3562bafc641593d172f5
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-03-02 13:21:19 +0400

    4

--- SQL_introduction/3-list_tables.sql
+++ SQL_introduction/3-list_tables.sql
@@ -1,3 +1,7 @@
 -- script that lists all tables in database named mysql
 
-SHOW TABLES FROM hbtn_0c_0;
+SHOW TABLES;
+
+# command line : mysql -hlocalhost -uroot -p mysql , 
+# therefore SHOW TABLES will look into db named mysql, or
+# whatever name is being passed as db in the command line 
