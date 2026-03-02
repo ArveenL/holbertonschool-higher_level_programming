f9948a94bf16f79ea5834c40458c1c60fcfdf5f2
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-03-02 12:30:22 +0400

    1

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,5 +1,3 @@
--- Write a script that creates the database hbtn_0c_0 in your MySQL server.
--- If the database hbtn_0c_0 already exists, your script should not fail
--- You are not allowed to use the SELECT or SHOW statements
+-- Write a script that creates the database hbtn_0c_0 in your MySQL server, if already exists, script cannot fail
 
 CREATE DATABASE <hbtn_0c_0>
