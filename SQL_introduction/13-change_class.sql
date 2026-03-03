6066b9906539f9b4deda228e4157f33f139525bf
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-03-03 12:32:55 +0400

    13

---
+++ SQL_introduction/13-change_class.sql
@@ -0,0 +1,6 @@
+-- Write a script that removes all records with a score <= 5 
+-- in the table second_table of the database hbtn_0c_0 in your MySQL server.
+
+FROM second_table
+DROP score
+WHERE score <= 5
