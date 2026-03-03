40a98b75b1c3087f3e826d409125d0bb06d4c7c1
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-03-03 11:14:24 +0400

    9

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,5 @@
 -- Write a script that creates a table second_table in the database hbtn_0c_0 in your MySQL server and add multiples rows.
 
 CREATE TABLE IF NOT EXISTS second_table(
-    Id INT,
-    name VARCHAR(256),
-    score INT
+    Id INT, name VARCHAR(256), score INT
 );
