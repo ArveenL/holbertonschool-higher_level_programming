2d1e0fe21e94a5bc3968c0b750bb13795dfb51a7
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-03-31 15:09:55 +0400

    5

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -7,9 +7,9 @@ if (isNaN(args[0])) {
 }
 
 else {
-    console.log('My number: parseInt$(args[0])');
+    console.log('My number: ${parseInt(args[0])}');
 }
 
 // isNaN is true if args cannot be converted into an integer, stands for : Is Not A Number
 // parseInt is to convert args into integer(not exactly but sort of)
-// $ so that it doesn't print literally the args but rather the conversion 
+// $ so that it doesn't print literally the args but rather the conversion. Used with {}
