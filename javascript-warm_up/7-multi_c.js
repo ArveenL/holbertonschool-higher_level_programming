d7a5deb590255526780cbe198d4a8cb98eb59d99
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-03-31 15:40:10 +0400

    7

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const args = process.argv.slice(2);
+const x = parseInt(process.argv[0]);
+
+if (isNaN(args[0])) {
+    console.log('Missing number of occurrences'); 
+}
+
+else {
+    for (let i = 0; i < x; i++) {
+        console.log('C is fun');
+    }
+}
