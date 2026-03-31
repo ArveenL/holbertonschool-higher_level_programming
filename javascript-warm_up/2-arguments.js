f0743607f94599e90c88b2ee4d54a56824418873
Arveen Loopesh Mogun <arveenloopeshmogun@Arveens-MacBook-Air.local>
2026-03-31 11:50:31 +0400

    2

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,14 +1,10 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
 
-if (argv.length === 0)  {
-    console.log('No argument');
-}
-
-else if (argv.lengtj === 1) {
-    console.log('Argument found');
-}
-
-else  {
-    console.log('Arguments found');
+if (args.length === 0) {
+  console.log('No argument');
+} else if (args.length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
